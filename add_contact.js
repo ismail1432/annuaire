1932b418d1d5d5bf50b33e8ea6b379fd449f9a2b
smaine M <contact@smaine.me>
2018-02-27 23:32:40 +0100

    add contact good

--- add_contact.js
+++ add_contact.js
@@ -1,1 +1,21 @@
-alert('ici');
+$('form').submit( function(e) {
+e.preventDefault();
+
+var firstname = $( "input[name=firstname]" ).val();
+var lastname = $( "input[name=lastname]" ).val();
+var creationDate = Date.now();
+
+var contactStored = JSON.parse(localStorage.getItem('contact')) || [];
+console.log(contactStored.length);
+
+contactObject = {
+	id : contactStored.length,
+	firstname : firstname,
+	lastname : lastname,
+	creationDate : creationDate
+};
+
+contactStored.push(contactObject);
+
+localStorage.setItem('contact', JSON.stringify(contactStored));
+});
